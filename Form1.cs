11f9d928a3dea3aeb7356a060ac43ed49a504de1
0385246651 <haibaba2k@gmail.com>
2025-05-12 11:15:30 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormGUI
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+
+        }
+
+        int i = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i++;
+            MessageBox.Show("Bạn đã click BTN " + i + " lần!");
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
